d0a99b81573f0c567ae56c8977bb25c5487e0cd8
dphale <dphale@pacbell.net>
2019-08-18 12:55:42 -0700

    Added bootstrap, success var

--- contracts/UserList.sol
+++ contracts/UserList.sol
@@ -1,6 +1,23 @@
 pragma solidity ^0.5.0;
 
 contract UserList {
-  uint public taskCount = 0;
+  uint public userCount = 0;
+
+  struct User {
+    uint listNum;
+    uint userID;
+    bool completed;
+  }
+
+  mapping(uint => User) public users;
+
+  constructor() public {
+    createUser(23576);
+  }
+
+  function createUser(uint _userID) public {
+    userCount ++;
+    users[userCount] = User(userCount, _userID, false);
+  }
 
 }
