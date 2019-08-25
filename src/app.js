948d2842e1c5b079990aa0e25f91bbe1398cc5b7
dphale <dphale@pacbell.net>
2019-08-24 17:00:59 -0700

    Updates to truffle config, app and index

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 App = {
-//  loading: false,
-//  contracts: {},
+
+  loading: false,
+  contracts: {},
 
   load: async () => {
     await App.loadWeb3()
     await App.loadAccount()
     await App.loadContract()
-    console.log("loadContract called...")
-//    await App.render()
+    // await App.render()
   },
 
 
@@ -26,7 +26,9 @@ App = {
         // Request account access if needed
         await ethereum.enable()
         // Acccounts now exposed
-        web3.eth.sendTransaction({/* ... */})
+        web3.eth.sendTransaction({
+          gas:100000
+        })
       } catch (error) {
         // User denied account access...
       }
@@ -36,7 +38,9 @@ App = {
       App.web3Provider = web3.currentProvider
       window.web3 = new Web3(web3.currentProvider)
       // Acccounts always exposed
-      web3.eth.sendTransaction({/* ... */})
+      web3.eth.sendTransaction({
+        gas:110000
+      })
     }
     // Non-dapp browsers...
     else {
@@ -47,101 +51,41 @@ App = {
   loadAccount: async () => {
     // Set the current blockchain account
     App.account = web3.eth.accounts[0]
-    console.log(App.account);
   },
 
   loadContract: async () => {
     // Create a JavaScript version of the smart contract
-    console.log("getting json")
-    const userList = await $.getJSON('./build/contracts/UserList.json')
-    // App.contracts.UserList = TruffleContract(userList)
-    // App.contracts.UserList.setProvider(App.web3Provider)
-    //console.log(userList)
+    const userList = await $.getJSON('../build/contracts/UserList.json')
+    App.contracts.UserList = TruffleContract(userList)
+    App.contracts.UserList.defaults({gasLimit:6721975})
+    App.contracts.UserList.setProvider(App.web3Provider)
     // Hydrate the smart contract with values from the blockchain
-    // App.userList = await App.contracts.UserList.deployed()
+    App.userList = await App.contracts.UserList.deployed()
+    console.log(App.userList)
   },
 
-
-//   render: async () => {
-//     // Prevent double render
-//     if (App.loading) {
-//       return
-//     }
-//
-//     // Update app loading state
-//     App.setLoading(true)
-//
-//     // Render Account
-//     $('#account').html(App.account)
-//
-//     // Render Tasks
-//     await App.renderTasks()
-//
-//     // Update loading state
-//     App.setLoading(false)
-//   },
-//
-//   renderTasks: async () => {
-//     // Load the total task count from the blockchain
-//     const taskCount = await App.todoList.taskCount()
-//     const $taskTemplate = $('.taskTemplate')
-//
-//     // Render out each task with a new task template
-//     for (var i = 1; i <= taskCount; i++) {
-//       // Fetch the task data from the blockchain
-//       const task = await App.todoList.tasks(i)
-//       const taskId = task[0].toNumber()
-//       const taskContent = task[1]
-//       const taskCompleted = task[2]
-//
-//       // Create the html for the task
-//       const $newTaskTemplate = $taskTemplate.clone()
-//       $newTaskTemplate.find('.content').html(taskContent)
-//       $newTaskTemplate.find('input')
-//                       .prop('name', taskId)
-//                       .prop('checked', taskCompleted)
-//                       .on('click', App.toggleCompleted)
-//
-//       // Put the task in the correct list
-//       if (taskCompleted) {
-//         $('#completedTaskList').append($newTaskTemplate)
-//       } else {
-//         $('#taskList').append($newTaskTemplate)
-//       }
-//
-//       // Show the task
-//       $newTaskTemplate.show()
-//     }
-//   },
-//
-//   createTask: async () => {
-//     App.setLoading(true)
-//     const content = $('#newTask').val()
-//     await App.todoList.createTask(content)
-//     window.location.reload()
-//   },
-//
-//   toggleCompleted: async (e) => {
-//     App.setLoading(true)
-//     const taskId = e.target.name
-//     await App.todoList.toggleCompleted(taskId)
-//     window.location.reload()
-//   },
-//
-//   setLoading: (boolean) => {
-//     App.loading = boolean
-//     const loader = $('#loader')
-//     const content = $('#content')
-//     if (boolean) {
-//       loader.show()
-//       content.hide()
-//     } else {
-//       loader.hide()
-//       content.show()
-//     }
-//   }
-// }
-
+  // render: async () => {
+  //   // Prevent double render
+  //   if (App.loading) {
+  //     return
+  //   }
+  //
+  //   // Update app loading state
+  //   App.setLoading(true)
+  //
+  //   // Render Account
+  //   $('#account').html(App.account)
+  //
+  //   // Render Tasks
+  //   // await App.renderTasks()
+  //
+  //   // Update loading state
+  //   App.setLoading(false)
+  //
+  // },
+  createUser: async () => {
+    await App.userList.createUser(nameCardID)
+  }
 }
 
 $(() => {
--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,9 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*" // Match any network id
+      network_id: "*", // Match any network id
+      gasPrice: 100000000000,
+      gas: 6721975 // gas limit
     }
   },
   solc: {
