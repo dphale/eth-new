03e659f4553c7d33b969ac1cafc81dab0f5f92eb
cmcgrath13 <cpmcgrat@gmail.com>
2019-09-10 14:34:18 -0700

    Added event for newuser for processing, added /contract endpoint for fetching event JSON for UI

--- server/app.py
+++ server/app.py
@@ -1,29 +1,44 @@
 from web3 import Web3
+from web3.middleware import geth_poa_middleware
 
 from flask import Flask, request
 from flask_cors import CORS
 
 import configparser
 import json
+from datetime import datetime
+from hexbytes import HexBytes
 
 app = Flask(__name__)
 CORS(app)
 
+class HexJsonEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, HexBytes):
+            return obj.hex()
+        return super().default(obj)
+
 class UserListController(object):
-    def __init__(self, ipc_file, contract_address, contract_json, address, keyfile, passwordfile, explorer_format):
+    def __init__(self, ipc_file, contract_address, contract_json, address, keyfile, passwordfile, explorer_format, publish_block):
         self.address = Web3.toChecksumAddress(address)
         self.explorer_format = explorer_format
 
         self.client = Web3(Web3.IPCProvider(ipc_file))
+        self.client.middleware_onion.inject(geth_poa_middleware, layer=0)
 
         self.private_key = self._load_private_key(keyfile, passwordfile)
 
         abi = self._generate_contract_abi(contract_json)
         self.contract = self._load_contract(abi, contract_address)
 
+        self.filter = self._load_filter(publish_block)
+
     def _generate_contract_abi(self, contract_json):
         with open(contract_json, "r") as abi_definition:
             return json.load(abi_definition)['abi']
+
+    def _load_filter(self, publish_block):
+        return self.contract.events.NewUser.createFilter(fromBlock=int(publish_block), toBlock="latest")
 
     def _load_contract(self, abi, contract_address):
         return self.client.eth.contract(contract_address, abi=abi)
@@ -43,11 +58,24 @@ class UserListController(object):
             "gas": 100000,
         }
 
+    def get_transaction_history(self):
+        history = self.filter.get_all_entries()
+        return history
+
+    def get_block(self, block_num):
+        return self.client.eth.getBlock(block_num)
+
+    def get_transaction(self, transaction_hash):
+        return self.client.eth.getTransaction(transaction_hash)
+
     def create_user(self, nameCardID):
         tx_data = self._base_tx()
         tx = self.contract.functions.createUser(nameCardID).buildTransaction(tx_data)
         signed_tx = self.client.eth.account.signTransaction(tx, self.private_key)
         return self.client.eth.sendRawTransaction(signed_tx.rawTransaction)
+
+    def get_gas_used(self, transaction_hash):
+        return self.client.eth.waitForTransactionReceipt(transaction_hash).gasUsed
 
     def get_explorer_url(self, txid):
         return self.explorer_format.format(tx=txid.hex())
@@ -56,7 +84,27 @@ class UserListController(object):
 config = configparser.ConfigParser()
 config.read('config.ini')
 contracting = config['contracting']
-ulc = UserListController(contracting['IPC'], contracting['ContractAddress'], contracting['ContractJSON'], contracting['Address'], contracting['KeyFile'], contracting['PasswordFile'], contracting['ExplorerFormat'])
+ulc = UserListController(contracting['IPC'], contracting['ContractAddress'], contracting['ContractJSON'], contracting['Address'], contracting['KeyFile'], contracting['PasswordFile'], contracting['ExplorerFormat'], contracting['PublishBlock'])
+
+@app.route('/contract', methods=['GET'])
+def get_contract_data():
+    history = ulc.get_transaction_history()
+    toReturn = {
+        "UserList": []
+    }
+    for event in history:
+        blockData = ulc.get_block(event['blockNumber'])
+        transactionData = ulc.get_transaction(event['transactionHash'])
+        gasUsed = ulc.get_gas_used(event['transactionHash'])
+        toReturn["UserList"].append({
+            "UserID": event['args']['_userID'],
+            "Timestamp": datetime.fromtimestamp(blockData['timestamp']).isoformat(),
+            "Block": event['blockNumber'],
+            "TxFee": "{} Ether".format((gasUsed * transactionData['gasPrice'])/1000000000000000000),
+            "Link": ulc.get_explorer_url(event['transactionHash'])
+        })
+    return toReturn
+
 
 @app.route('/newuser', methods=['POST'])
 def create_user():
@@ -70,4 +118,4 @@ def create_user():
 if __name__ == "__main__":
     # This is a test creation invoked directly, this should be replaced with a web server
     #ulc.create_user(999999)
-    app.run(port=443, host='0.0.0.0', debug=True)
+    app.run(port=80, host='0.0.0.0', debug=True)
